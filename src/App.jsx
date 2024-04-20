8074694cdb5c142a78098fb065d6d004c6bb858b
TomChaseParis <chasethomasparis@gmail.com>
2024-04-20 10:50:37 +0200

    feat: Create Programs component & Title component used inside the Programs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar'
-import Hero from './components/hero/Hero'
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+import Programs from "./components/programs/Programs";
+import Title from "./components/title/Title";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer' />
+        <Programs />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
