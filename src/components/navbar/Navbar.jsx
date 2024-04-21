37d3051dcaccd28516d97c8f077979b73d85922f
TomChaseParis <chasethomasparis@gmail.com>
2024-04-21 10:05:33 +0200

    feat: Add burger menu and enhance Navbar responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import menu_icon from "../../assets/menu-icon.png"
 import { Link } from "react-scroll";
 
 const Navbar = () => {
@@ -12,10 +13,15 @@ const Navbar = () => {
     });
   }, []);
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
-      <ul>
+      <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
         <li>
           <Link to="hero" smooth={true} offset={0} duration={500}>
             Home
@@ -43,9 +49,18 @@ const Navbar = () => {
         </li>
 
         <li>
-          <Link to="contact" smooth={true} offset={-260} duration={500} className="btn">Contact us</Link>
+          <Link
+            to="contact"
+            smooth={true}
+            offset={-260}
+            duration={500}
+            className="btn"
+          >
+            Contact us
+          </Link>
         </li>
       </ul>
+      <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
     </nav>
   );
 };
