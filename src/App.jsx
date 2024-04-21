5a098dcecb3b6694e1462f97244086a22e8d5ac4
TomChaseParis <chasethomasparis@gmail.com>
2024-04-21 11:33:50 +0200

    feat: Create VideoPlayer component used inside About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Programs from "./components/programs/Programs";
@@ -8,24 +8,28 @@ import Campus from "./components/campus/Campus";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import VideoPlayer from "./components/videoplayer/VideoPlayer";
 
 const App = () => {
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar />
       <Hero />
       <div className="container">
-        <Title subTitle='Our PROGRAM' title='What We Offer' />
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
         <Programs />
-        <About />
-        <Title subTitle='Gallery' title='Campus Photos' />
+        <About setPlayState={setPlayState} />
+        <Title subTitle="Gallery" title="Campus Photos" />
         <Campus />
-        <Title subTitle='TESTIMONIALS' title='What Student Says' />
+        <Title subTitle="TESTIMONIALS" title="What Student Says" />
         <Testimonials />
-        <Title subTitle='Contact Us' title='Get in Touch' />
+        <Title subTitle="Contact Us" title="Get in Touch" />
         <Contact />
         <Footer />
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   );
 };
